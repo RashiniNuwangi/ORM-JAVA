0187b8e0bf03d8b67450084a1e43c7e9ff27f2f1
Rashini Nuwangi <119997820+RashiniNuwangi@users.noreply.github.com>
2024-03-25 11:21:31 +0530

    Initial commit

---
+++ src/main/java/Users.java
@@ -0,0 +1,49 @@
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+
+@Entity
+@Table(name="users")
+public class Users
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userid;
+
+    private String fullname;
+
+    private String email;
+
+    private String password;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
